6a46b06727c44002945d22e0653b0bd18da3f911
Luis Carrasco <lclucas84@gmail.com>
2021-01-11 17:05:06 +0100

    version final con javadoc DNI 79105097z

--- src/empleadoed_79105087z/EmpleadoED_79105087z.java
+++ src/empleadoed_79105087z/EmpleadoED_79105087z.java
@@ -1,28 +1,45 @@
 package empleadoed_79105087z;
 
 import java.util.*;
-
+/**
+ * Ejercicio práctico examen Eval 1 ED
+ * @author Luis Carrasco DNI 79105087Z
+ * @version 1.0
+ */
 public class EmpleadoED_79105087z {
-static final double FACTOR_HORA_EXTRA = 1.2;
-private String nombreCompleto, dni;
-private double salarioBasePorHora = 10.56;
-public EmpleadoED_79105087z(String nombreCompleto, String dni) {
-this.nombreCompleto = nombreCompleto;
-this.dni = dni;
-}
-public double getSalarioEsteMes(int horasExtra) {
-double cantidadExtra, salarioFinal;
-cantidadExtra = horasExtra * this.salarioBasePorHora *
-FACTOR_HORA_EXTRA;
-salarioFinal=this.salarioBasePorHora * 40 * 4 + cantidadExtra;
-salarioFinal=(double)Math.round(salarioFinal * 100d) / 100d;
-return salarioFinal;
-}
+ 
+    static final double FACTOR_HORA_EXTRA = 1.2;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+    /**
+     * Contructor con tres parámetros
+     * @param nombreCompleto nombre completo en mayúsculas
+     * @param dni documento nacional de identidad con letra y sin espacios
+     */
+    public EmpleadoED_79105087z(String nombreCompleto, String dni) {
+        this.nombreCompleto = nombreCompleto;
+        this.dni = dni;
+    }
+    /**
+     * Calcula el salario bruto mansual en función del salario base por hora y las horas extras
+     * El precio por hora de la hora extra se determina con la constante FACTOR_HORA_EXTRA
+     * @param horasExtra número de horas extra dedicadas redondeado a la baja (entero)
+     * @return devuelve el salario bruto mensual en euros con dos decimales
+     */
+    public double getSalarioEsteMes(int horasExtra) {
+        double cantidadExtra, salarioFinal;
+        cantidadExtra = horasExtra * this.salarioBasePorHora
+                * FACTOR_HORA_EXTRA;
+        salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+        salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
+        return salarioFinal;
+    }
 // Este método no lo comentaremos con JavaDoc
-public static void main(String[] args) {
+
+    public static void main(String[] args) {
 // !!!! MODIFICA TU NOMBRE Y TU DNI EN LA SIGUIENTE LÍNEA
-EmpleadoED_79105087z emp = new EmpleadoED_79105087z("Luis Carrasco","79105087Z");
-int horasExtra = 2;
-System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra) + " euros, al hacer " + horasExtra + " horas extra.");
-}
+        EmpleadoED_79105087z emp = new EmpleadoED_79105087z("Luis Carrasco", "79105087Z");
+        int horasExtra = 2;
+        System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra) + " euros, al hacer " + horasExtra + " horas extra.");
+    }
 }
